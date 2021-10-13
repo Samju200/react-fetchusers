d135fb54459134bcd2177442ae7662caaf5c0950
Adeyemi juwon <samju7778@gmail.com>
2021-10-13 19:15:13 +0100

    htyptcript

--- src/component/pagination.js
+++ src/component/pagination.js
@@ -1,46 +1,51 @@
-import React from 'react'
+import React from 'react';
 import './pagination.css';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
-const Pagination = ({postsPerPage, totalPosts, paginate, nextPage,prevPage }) => {
- const pageNumbers = [];
- for(let i= 1; i <= Math.ceil(totalPosts/postsPerPage); i++){
-     pageNumbers.push(i)
- }
-    return (
-      <div className="pagination">
-        <button
-          className="prev-btn"
-          onClick={() => {
-            prevPage();
-          }}
-        >
-          <ArrowBackIosIcon className="page-btn" />
-        </button>
-        {/* {pageNumbers.map((number) => {
-          return (
-            <button
-              key={number}
-              className="page-item"
-              onClick={() => {
-                paginate(number);
-              }}
-            >
-              {number}
-            </button>
-          );
-        })} */}
-        <button
-          className="prev-btn"
-          onClick={() => {
-            nextPage();
-          }}
-        >
-          <ArrowForwardIosIcon className="page-btn" />
-        </button>
-        
-      </div>
-    );
-}
+const Pagination = ({
+  postsPerPage,
+  totalPosts,
+  paginate,
+  nextPage,
+  prevPage,
+}) => {
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+  return (
+    <div className="pagination">
+      <button
+        className="prev-btn"
+        onClick={() => {
+          prevPage();
+        }}
+      >
+        <ArrowBackIosIcon className="page-btn" />
+      </button>
+      {pageNumbers.map((number) => {
+        return (
+          <button
+            key={number}
+            className="page-item"
+            onClick={() => {
+              paginate(number);
+            }}
+          >
+            {number}
+          </button>
+        );
+      })}
+      <button
+        className="prev-btn"
+        onClick={() => {
+          nextPage();
+        }}
+      >
+        <ArrowForwardIosIcon className="page-btn" />
+      </button>
+    </div>
+  );
+};
 
-export default Pagination
+export default Pagination;
